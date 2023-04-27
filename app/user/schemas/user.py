b982bc8dc1ad786d6e21fa2687dfcc76dc2fb940
rom.sinitsyn <rom.sinitsyn@x5.ru>
2023-04-27 19:26:24 +0300

    init project

---
+++ app/user/schemas/user.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, Field
+
+
+class GetUserListResponseSchema(BaseModel):
+    id: int = Field(..., description="Id")
+    email: str = Field(..., description="Email")
+    nickname: str = Field(..., description="Nickname")
+    rating: int = Field(..., description="Rating")
+
+    class Config:
+        orm_mode = True
+
+
+class CreateUserRequestSchema(BaseModel):
+    email: str = Field(..., description="Email")
+    password1: str = Field(..., description="Password1")
+    password2: str = Field(..., description="Password2")
+    nickname: str = Field(..., description="Nickname")
+
+
+class CreateUserResponseSchema(BaseModel):
+    email: str = Field(..., description="Email")
+    nickname: str = Field(..., description="Nickname")
+
+    class Config:
+        orm_mode = True
+
+
+class LoginResponseSchema(BaseModel):
+    token: str = Field(..., description="Token")
+    refresh_token: str = Field(..., description="Refresh token")
