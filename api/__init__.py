e7d6d15ba120fc80424785b09a6566fb25646471
rom.sinitsyn <rom.sinitsyn@x5.ru>
2023-04-29 00:44:12 +0300

    FEATURE: added event entity

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
 from api.user.v1.user import user_router as user_v1_router
+from api.event.v1.event import event_router as event_v1_router
 from api.auth.auth import auth_router
 
 router = APIRouter()
 router.include_router(user_v1_router, prefix="/api/v1/users", tags=["User"])
+router.include_router(event_v1_router, prefix="/api/v1/events", tags=["Event"])
 router.include_router(auth_router, prefix="/auth", tags=["Auth"])
 
 
---
+++ app/event/schemas/event.py
@@ -0,0 +1,38 @@
+import datetime
+from typing import List
+
+from pydantic import BaseModel, Field
+from app.user.schemas import CreateUserResponseSchema
+
+
+class GetEventListResponseSchema(BaseModel):
+    id: int = Field(..., description="Id")
+    title: str = Field(..., description="Title")
+    description: str = Field(..., description="Description")
+    rating: int = Field(..., description="Rating")
+
+    class Config:
+        orm_mode = True
+
+
+class CreateEventRequestSchema(BaseModel):
+    title: str = Field(..., description="title")
+    description: str = Field(..., description="description")
+    link: str = Field(..., description="link")
+    date_start: datetime.date = Field(..., description="date_start")
+    limit_member: int = Field(..., description="limit_member")
+    location: str = Field(..., description="location")
+    longitude: float = Field(..., description="longitude")
+    latitude: float = Field(..., description="latitude")
+
+
+class CreateEventResponseSchema(BaseModel):
+    id: int = Field(..., description="Id")
+    title: str = Field(..., description="Title")
+    description: str = Field(..., description="Description")
+    rating: int = Field(..., description="Rating")
+    organizators: List[CreateUserResponseSchema]
+    members: List[CreateUserResponseSchema]
+
+    class Config:
+        orm_mode = True
